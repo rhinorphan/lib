575892135daf9ea592fc6c26873a8649f9c67397
Axel Tarifa <axel.tarifa@gmail.com>
2021-04-15 08:00:32 +0200

    fifth commit // 03_stairway.rb linking result_dice method to average_finish_time method

--- 03_stairway.rb
+++ 03_stairway.rb
@@ -1,6 +1,6 @@
 def dice_game
   puts "Bienvenue dans le jeux du dés !"
-  puts "Veuillez lancer le dé en appuyant sur une touche et entré"
+  puts "Veuillez lancer le dé en appuyant sur la touche entré"
   dice = gets.chomp
 end
 
@@ -24,33 +24,17 @@ def result_dice
     puts " "
     puts "BRAAAAAVVVOOOOOOO !!!!! Tu as gagné le jeux ! Pour info, tu as finis la partie en #{dice_thrown} tours !"
   end
+  return dice_thrown
 end
 
-def average_finish_time
+def average_finish_time(result_dice)
+  dice_thrown = result_dice
   dice_thrown_count = []
   puts "Combien de parties voulez-vous rejouer ?"
   print "> "
   number_of_games = gets.to_i
   number_of_games.times do 
-    steps = 0
-    dice_thrown = 0
-    until steps == 10
-      dice_result = rand(1..6)
-      if dice_result == 5 or dice_result == 6
-        steps += 1
-        puts "Bravo, tu avances d'une marche ! Tu te trouves sur la marche #{steps}."
-      elsif dice_result == 1
-        steps -=1
-        puts "Dommage, tu descends d'une marche.. Tu te trouves sur la marche #{steps}."
-      else dice_result == 2 or dice_result == 3 or dice_result == 4
-        puts "Tu restes sur place. Tu te trouves sur la marche #{steps}."
-      end
-      dice_thrown += 1
-    end
-    if steps == 10
-      puts " "
-      puts "BRAAAAAVVVOOOOOOO !!!!! Tu as gagné le jeux ! Pour info, tu as finis la partie en #{dice_thrown} tours !"
-    end
+    result_dice
     dice_thrown_count.push(dice_thrown)
   end
   total = dice_thrown_count.sum
@@ -61,7 +45,7 @@ end
 def perform
   dice_game
   result_dice
-  average_finish_time
+  average_finish_time(result_dice)
 end
 
 perform
